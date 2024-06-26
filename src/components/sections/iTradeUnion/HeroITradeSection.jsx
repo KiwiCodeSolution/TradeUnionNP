fc4df203af9d7bed83cb20488e752bb9f6f39b22
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2024-06-26 21:50:54 +0300

    ITrade

---
+++ src/components/sections/iTradeUnion/HeroITradeSection.jsx
@@ -0,0 +1,15 @@
+import BaseSection from "@/components/BaseSection";
+import PathPage from "@/components/PathPage";
+import Wrapper from "@/components/Wrapper";
+
+const HeroITradeSection = () => {
+  return (
+    <BaseSection style={"bg-bgGrey"}>
+      <Wrapper>
+        <PathPage endPath={"Я - Профспілка"} />
+      </Wrapper>
+    </BaseSection>
+  );
+};
+
+export default HeroITradeSection;
