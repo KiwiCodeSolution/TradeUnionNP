d6d50abc6e7ed9e274526a789979a811a65cce07
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2024-07-23 17:53:06 +0300

    TemplatesList

--- src/components/sections/templates/Blank.jsx
+++ src/components/sections/templates/Blank.jsx
@@ -1,7 +1,9 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Bookmark from "@/images/bookmark.svg";
 
-const Blank = ({ item }) => {
+const Blank = ({ item, index }) => {
+  const t = useTranslations("Forms");
   return (
     <article className="w-full bg-white rounded-2xl px-9 py-6 flex flex-col justify-between gap-y-4 relative">
       <Image
@@ -11,14 +13,14 @@ const Blank = ({ item }) => {
         alt="червона закладка"
         className="absolute -top-1 left-8"
       />
-      <h3 className="text-2xl font-bold">{item.title}</h3>
+      <h3 className="text-2xl font-bold">{t(`templates.${index}`)}</h3>
       <div className="flex flex-col gap-y-4">
         {item.text && <p className="text-lg text-grey">{item.text}</p>}
         <a
           href={item.file}
           className="text-lg text-red underline underline-offset-1 hover:underline-offset-4"
         >
-          Завантажити заяву
+          {t(`link`)}
           <span className="no-underline"> &gt;</span>
         </a>
       </div>
--- src/components/sections/templates/TemplatesList.jsx
+++ src/components/sections/templates/TemplatesList.jsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import templates from "@/data/templates.json";
 import BaseSection from "@/components/BaseSection";
 import Wrapper from "@/components/Wrapper";
@@ -6,26 +7,28 @@ import PathPage from "@/components/PathPage";
 import Title from "@/components/Title";
 
 const TemplatesList = () => {
+  const t = useTranslations("Forms");
   return (
     <BaseSection style={"bg-bgGrey"}>
       <Wrapper styles={"flex flex-col"}>
-        <PathPage endPath={"Бланки"} />
+        <PathPage endPath={t(`path`)} />
 
         <Title tag="h2" styles="hidden md:inline-block text-center mb-8">
-          <span className="text-red">Бланки</span> на вступ до <br /> Профспілки
+          <span className="text-red">{t(`title_templates.0`)}</span> {t(`title_templates.1`)}
+          <br /> {t(`title_templates.2`)}
         </Title>
         <Title tag="h2" styles="md:hidden inline-block text-center mb-8">
-          <span className="text-red">Бланки</span> на вступ до Профспілки
+          <span className="text-red">{t(`title_templates.0`)}</span> {t(`title_templates.1`)}
+          {t(`title_templates.2`)}
         </Title>
         <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
-          {templates.map(el => (
-            <Blank key={el.title} item={el} />
+          {templates.map((el, index) => (
+            <Blank key={el.file} item={el} index={index} />
           ))}
         </div>
         <p className="text-lg text-grey mt-8 text-center">
-          <span className="text-red">Завантажте</span> Заяву на вступ,{" "}
-          <span className="text-red">роздрукуйте та надішліть</span> за шаблоном “Матеріальна
-          допомога від Профспілки”
+          <span className="text-red">{t(`text_templates.0`)}</span> {t(`text_templates.1`)}
+          <span className="text-red">{t(`text_templates.2`)}</span> {t(`text_templates.3`)}
         </p>
       </Wrapper>
     </BaseSection>
