904868cf0c0c5472f13728df5672f8caa6db856a
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-02-05 21:40:22 +0200

    update image url

--- src/components/sections/news/NewsItem.jsx
+++ src/components/sections/news/NewsItem.jsx
@@ -6,13 +6,13 @@ import NoImage from "@/images/No_Image.jpg";
 import { useState } from "react";
 import { Link } from "@/navigation";
 import { extractFirstImage } from "@/utils/extractFirstImage";
-import { BaseURLImage } from "@/constants/BaseUrl";
 
 const NewsItem = ({ item, section, onToggleArchive, onDelete, part, locale }) => {
   // шукаємо першу картинку у контенті
 
   const imageItemLink = extractFirstImage(item.content);
-  const imagePreviewImg = `${BaseURLImage}${item.previewImg}`;
+
+  console.log(imageItemLink);
 
   const [imageSrc, setImageSrc] = useState(imageItemLink || NoImage);
 
--- src/utils/extractFirstImage.js
+++ src/utils/extractFirstImage.js
@@ -1,24 +1,9 @@
-// export function extractFirstImage(content) {
-//   const imgRegex = /<img\s[^>]*src="([^"]*)"/i;
-//   const match = content.match(imgRegex);
-//   return match ? match[1] : null;
-// }
-
 export function extractFirstImage(content) {
   const imgRegex = /<img\s[^>]*src="([^"]*)"/i;
   const match = content.match(imgRegex);
-
-  if (match) {
-    let imageUrl = match[1];
-
-    // Якщо шлях починається з "../../.." або подібного, очищаємо його до /uploads
-    const relativePathPattern = /^(\.\.\/){3,}/; // Для шляху типу "../../../"
-    if (relativePathPattern.test(imageUrl)) {
-      imageUrl = imageUrl.replace(relativePathPattern, "/uploads");
-    }
-
-    return imageUrl;
+  const relativePathPattern = /^(\.\.\/){3,}/; // Для шляху типу "../../../"
+  if (relativePathPattern.test(match[1])) {
+    return null;
   }
-
-  return null;
+  return match ? match[1] : null;
 }
