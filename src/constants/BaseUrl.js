b81a53d24c9ffefd7f3ac98174daea131dea50d0
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-02-09 14:52:46 +0200

    bug fix img

--- src/constants/BaseUrl.js
+++ src/constants/BaseUrl.js
@@ -1,3 +1,5 @@
-export const BaseURL = "http://localhost:5000/api/";
-export const BaseURLImage = "http://localhost:5000";
-// export const BaseURL = "https://kiwicode.tech/api/";
+// export const BaseURL = "http://localhost:5000/api/";
+// export const BaseURLImage = "http://localhost:5000";
+export const BaseURLImage = "https://kiwicode.digital";
+
+export const BaseURL = "https://kiwicode.digital/bc/";
--- src/utils/extractFirstImage.js
+++ src/utils/extractFirstImage.js
@@ -2,7 +2,7 @@ export function extractFirstImage(content) {
   const imgRegex = /<img\s[^>]*src="([^"]*)"/i;
   const match = content.match(imgRegex);
   const relativePathPattern = /^(\.\.\/){3,}/; // Для шляху типу "../../../"
-  if (relativePathPattern.test(match[1])) {
+  if (match && relativePathPattern.test(match[1])) {
     return null;
   }
   return match ? match[1] : null;
