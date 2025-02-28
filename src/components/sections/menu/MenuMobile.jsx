5a17d062aa9a087dd54d6f316756ceac7f490faa
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-02-28 09:08:27 +0200

    update mob menu

--- src/components/sections/menu/MenuMobile.jsx
+++ src/components/sections/menu/MenuMobile.jsx
@@ -37,41 +37,47 @@ const MenuMobile = ({ locale }) => {
     {
       name: t(`aboutPageLinks.0.title`),
       link: "/kolektyvnyy-dohovir",
+      aria: "посилання на сторінку про колективну угоду",
     },
     {
       name: t(`aboutPageLinks.1.title`),
-      link: "/ppo",
-      aria: "посилання на сторінку колективних договорів",
+      link: "/yak-vstupyty",
+      aria: "посилання на сторінку про те, як вступити у Профспілку",
     },
     {
       name: t(`aboutPageLinks.2.title`),
-      link: "/zakhyst-trudovykh-prav",
-      aria: "посилання на сторінку колективних договорів",
+      link: "/ppo",
+      aria: "посилання на сторінку регіональних підрозділів",
     },
     {
       name: t(`aboutPageLinks.3.title`),
-      link: "/materialna-dopomoga",
-      aria: "посилання на сторінку колективних договорів",
+      link: "/zakhyst-trudovykh-prav",
+      aria: "посилання на сторінку про захист трудових прав",
     },
     {
       name: t(`aboutPageLinks.4.title`),
-      link: "/dozvillya-ta-sport",
-      aria: "",
+      link: "/materialna-dopomoga",
+      aria: "посилання на сторінку про матеріальну допомогу",
     },
     {
       name: t(`aboutPageLinks.5.title`),
-      link: "/ya-profspilka",
-      aria: "",
+      link: "/dozvillya-ta-sport",
+      aria: "посилання на сторінку дозвілля та спорт",
     },
     {
       name: t(`aboutPageLinks.6.title`),
-      link: "/zvernennya",
-      aria: "",
+      link: "/ya-profspilka",
+      aria: "посилання на сторінку",
     },
     {
       name: t(`aboutPageLinks.7.title`),
+      link: "/zvernennya",
+      aria: "посилання на сторінку про звернення учасників",
+    },
+    {
+      name: t(`aboutPageLinks.8.title`),
       link: "/ozdorovlennya",
-      aria: "",
+      aria: "посилання на сторінку про варіанти для оздоровлення",
     },
   ];
 
