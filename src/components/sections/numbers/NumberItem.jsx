08b73cd9455da3d061ee17afe0726e160eb87c9c
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-02-28 09:19:26 +0200

    update text and numbers home page

--- src/components/sections/numbers/NumberItem.jsx
+++ src/components/sections/numbers/NumberItem.jsx
@@ -72,7 +72,7 @@ const NumberItem = ({ item, index, text }) => {
                 formattingFn={formatNumber}
                 className="hidden xl:block"
               />
-              <CountUp start={0} end={30} className="xl:hidden" suffix="млн" />
+              <CountUp start={0} end={34} className="xl:hidden" suffix="млн" />
             </>
           ) : (
             <CountUp start={item.start} end={item.end} formattingFn={formatNumberToString} />
