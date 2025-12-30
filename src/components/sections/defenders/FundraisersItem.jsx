ba4409ef06f04f5942f7345ebb65a6f8fd241ec7
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-12-30 11:45:03 +0200

    оновлено розміри карток зборів, виправлено текст

--- src/components/sections/defenders/FundraisersItem.jsx
+++ src/components/sections/defenders/FundraisersItem.jsx
@@ -12,10 +12,10 @@ const FundraisersItem = ({ item, locale, section = "client" }) => {
 
   return (
     <article
-      className={`w-full flex flex-col justify-between bg-white md:pb-10 xl:pb-4 ${
+      className={`w-full h-full flex flex-col justify-between bg-white md:pb-10 xl:pb-4 ${
         section !== "client"
-          ? "shadow-standardShadow rounded-[32px] max-w-[204px] h-[436px] overflow-hidden border-[1px] border-black/10 relative z-10"
-          : "h-full"
+          ? "shadow-standardShadow rounded-[32px] max-w-[204px] overflow-hidden border-[1px] border-black/10 relative z-10"
+          : ""
       }`}
     >
       {section === "admin" && <FunctionalButtons item={item} token={token} />}
@@ -29,7 +29,7 @@ const FundraisersItem = ({ item, locale, section = "client" }) => {
           className="w-full min-w-[204px] max-w-[280p] h-full object-cover"
         />
       </div>
-      <div className="flex flex-col gap-y-2 p-2 pb-4 justify-between h-[calc(100%-146px)]">
+      <div className="flex flex-col gap-y-2 px-2 pt-2 justify-between flex-1">
         <p className="text-xl font-bold line-clamp-2">
           {locale === "uk" ? item.titleUk : item.titleEn}
         </p>
